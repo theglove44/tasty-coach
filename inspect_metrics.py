2bc000a4a1f8af308f7dfeccc9828203a388c750
theglove44 <thesage01@icloud.com>
2026-01-20 16:31:07 +0000

    Added `position_report.png` and deleted `inspect_metrics.py`.

--- inspect_metrics.py
+++
@@ -1,9 +0,0 @@
-
-from tastytrade import metrics
-from tastytrade.metrics import MarketMetricInfo
-
-print("=== MarketMetricInfo fields ===")
-try:
-    print(MarketMetricInfo.model_fields.keys())
-except:
-    print(dir(MarketMetricInfo))
